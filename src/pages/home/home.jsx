3e7bf6978fb9793bf1295df45950a42d88e6023b
furuchan63 <furuchan6666@gmail.com>
2020-06-07 00:08:47 +0900

    aaa

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -9,7 +9,7 @@ export default class Home extends React.Component {
         <div classNmae="bg_text_box">
         <img src="./aoyama02.png" width="100%"/>
         <div className="bg_introductiontext">
-        <p className="bg_text_box_h1">Yuki Furutani🐶</p>
+        <p className="bg_text_box_h1">Yuki Furutani</p>
         <p className="bg_text_box_p1"> a student at Doshisha university & an intern at Linc'well inc.</p>
         </div>
         </div>
